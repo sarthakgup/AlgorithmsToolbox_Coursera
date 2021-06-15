998c23e76344d4cc353274892fd9bd7fa703e924
sarthakgup <16374350+sarthakgup@users.noreply.github.com>
2021-06-14 19:44:19 -0700

    added comments for python file

--- APlusB.py
+++ APlusB.py
@@ -1,9 +1,13 @@
 # python3
 
+# taking a look at python version too
+# i need to improve my python skills
 
+# def indicates new user defined function aka method
 def sum_of_two_digits(first_digit, second_digit):
     return first_digit + second_digit
 
+# main method header
 if __name__ == '__main__':
     a, b = map(int, input().split())
     print(sum_of_two_digits(a, b))
