260dc58d6f76751863a4483672231b3e35cf68ba
sarthakgup <16374350+sarthakgup@users.noreply.github.com>
2021-06-19 22:56:53 -0700

    update

--- MaxPairwiseProduct.java
+++ MaxPairwiseProduct.java
@@ -1,62 +1,53 @@
 import java.util.*;
 import java.io.*;
 
-//Starter Code:
+public class MaxPairwiseProduct 
+{
+static int getMaxPairwiseProduct(int[] numbers) {
+    int product = 0;
+    int n = numbers.length;
 
-public class MaxPairwiseProduct {
-    static int getMaxPairwiseProduct(int[] numbers)
-    {
-        int max_product = 0;
-        int n = numbers.length;
-
-        for (int first = 0; first < n; ++first)
-        {
-            for (int second = first + 1; second < n; ++second) 
-            {
-                max_product = Math.max(max_product, numbers[first] * numbers[second]);
-            }
-        }
-
-        return max_product;
+    for (int i = 0; i < n; ++i) {
+    for (int j = i + 1; j < n; ++j) {
+    product = Math.max(product,
+    numbers[i] * numbers[j]);
     }
-
-    public static void main(String[] args) {
-        FastScanner scanner = new FastScanner(System.in);
-        int n = scanner.nextInt();
-        int[] numbers = new int[n];
-        for (int i = 0; i < n; i++) {
-            numbers[i] = scanner.nextInt();
-        }
-        System.out.println(getMaxPairwiseProduct(numbers));
     }
+    return product;
+}
 
-    static class FastScanner {
-        BufferedReader br;
-        StringTokenizer st;
-
-        FastScanner(InputStream stream) {
-            try {
-                br = new BufferedReader(new
-                    InputStreamReader(stream));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        String next() {
-            while (st == null || !st.hasMoreTokens()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
-        }
-
-        int nextInt() {
-            return Integer.parseInt(next());
-        }
-    }
-
+public static void main(String[] args) {
+FastScanner scanner = new FastScanner(System.in);
+int n = scanner.nextInt();
+int[] numbers = new int[n];
+for (int i = 0; i < n; i++) {
+numbers[i] = scanner.nextInt();
+}
+System.out.println(getMaxPairwiseProduct(numbers));
+}
+static class FastScanner {
+BufferedReader br;
+StringTokenizer st;
+FastScanner(InputStream stream) {
+try {
+br = new BufferedReader(new
+InputStreamReader(stream));
+} catch (Exception e) {
+e.printStackTrace();
+}
+}
+String next() {
+while (st == null || !st.hasMoreTokens()) {
+try {
+st = new StringTokenizer(br.readLine());
+} catch (IOException e) {
+e.printStackTrace();
+}
+}
+return st.nextToken();
+}
+int nextInt() {
+return Integer.parseInt(next());
+}
+}
 }
