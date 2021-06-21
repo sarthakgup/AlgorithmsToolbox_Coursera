031397186384b242f7ce2bc0c9beebc1757ec225
sarthakgup <16374350+sarthakgup@users.noreply.github.com>
2021-06-20 18:41:45 -0700

    updated final MaxPairwiseProduct.cpp

--- MaxPairwiseProduct.cpp
+++ MaxPairwiseProduct.cpp
@@ -1,30 +1,99 @@
+#include <cstdlib>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
-int64_t MaxPairwiseProduct(const std::vector<int64_t>& numbers) {
-    int64_t max_product = 0;
+using std::vector;
+using std::cin;
+using std::cout;
+
+long long MaxPairwiseProduct(const vector<int>& numbers)
+{
+    long long result = 0;
     int n = numbers.size();
-
-    for (int first = 0; first < n; ++first)
+    for(int i = 0; i < n; ++i)
     {
-        for (int second = first + 1; second < n; ++second)
+        for(int j =i+1; j<n; ++j)
         {
-            max_product = std::max(max_product, numbers[first] * numbers[second]);
+            if(((long long)numbers[i])*numbers[j] > result)
+            {
+                result = ((long long)numbers[i])*numbers[j];
+            }
         }
     }
 
-    return max_product;
+    return result;
 }
 
-int main() {
-    int n;
-    std::cin >> n;
-    std::vector<int64_t> numbers(n);
-    for (int i = 0; i < n; ++i) {
-        std::cin >> numbers[i];
+long long MaxPairwiseProductFast(const vector<int>& numbers)
+{
+    int n = numbers.size();
+
+    int max_index1 = -1;
+
+    for(int i = 0; i < n; ++i)
+    {
+        if((max_index1 == -1) || (numbers[i]> numbers[max_index1]))
+        {
+            max_index1 = i;
+        }
     }
 
-    std::cout << MaxPairwiseProduct(numbers) << "\n";
+    int max_index2 = -1;
+
+    for(int j = 0; j < n; ++j)
+    {
+        if((j != max_index1) && ((max_index2 == -1) || (numbers[j] > numbers[max_index2])))
+        {
+            max_index2 = j;
+        }
+    }
+
+    return ((long long)(numbers[max_index1])) * numbers[max_index2];
+}
+
+int main()
+{
+    //STRESS TEST IMPLEMENTATION BELOW:
+    /*
+    while(true)
+    {
+        int n = rand() % 10 + 2;
+        cout << n << "\n";
+        vector<int> a;
+
+        for(int i = 0; i < n; ++i)
+        {
+            a.push_back(rand() % 100000);
+        }
+
+        for(int i = 0; i < n; ++i)
+        {
+            cout << a[i] << ' ';
+        }
+
+        cout << "\n";
+        long long res1 = MaxPairwiseProduct(a);
+        long long res2 = MaxPairwiseProductFast(a);
+
+        if(res1 != res2)
+        {
+            cout << "Wrong answer: " << res1 << ' ' << res2 << "\n";
+            break;
+        }
+        else{
+            cout << "OK\n";
+        }
+    }
+*/
+    int n;
+    cin >> n;
+    vector<int> numbers(n);
+    for(int i = 0; i < n; ++i)
+    {
+        cin >> numbers[i];
+    }
+
+    long long result = MaxPairwiseProductFast(numbers);
+    cout << result << "\n";
     return 0;
 }
