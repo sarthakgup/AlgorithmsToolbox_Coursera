0ed84232ae01bab6d3fc631e60682c40051f6eeb
sarthakgup <16374350+sarthakgup@users.noreply.github.com>
2021-06-20 17:39:55 -0700

    updates

--- MaxPairwiseProduct.cpp
+++ MaxPairwiseProduct.cpp
@@ -2,8 +2,8 @@
 #include <vector>
 #include <algorithm>
 
-int MaxPairwiseProduct(const std::vector<int>& numbers) {
-    int max_product = 0;
+int64_t MaxPairwiseProduct(const std::vector<int64_t>& numbers) {
+    int64_t max_product = 0;
     int n = numbers.size();
 
     for (int first = 0; first < n; ++first)
@@ -20,7 +20,7 @@ int MaxPairwiseProduct(const std::vector<int>& numbers) {
 int main() {
     int n;
     std::cin >> n;
-    std::vector<int> numbers(n);
+    std::vector<int64_t> numbers(n);
     for (int i = 0; i < n; ++i) {
         std::cin >> numbers[i];
     }
