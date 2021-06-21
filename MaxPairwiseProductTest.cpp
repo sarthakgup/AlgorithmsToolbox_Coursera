38814dcc33584fde3d1d1a64ed41e43b45201d8c
sarthakgup <16374350+sarthakgup@users.noreply.github.com>
2021-06-20 18:38:49 -0700

    Stress test implementation

--- MaxPairwiseProductTest.cpp
+++ MaxPairwiseProductTest.cpp
@@ -1,3 +1,4 @@
+#include <cstdlib>
 #include <iostream>
 #include <vector>
 
@@ -41,7 +42,7 @@ long long MaxPairwiseProductFast(const vector<int>& numbers)
 
     for(int j = 0; j < n; ++j)
     {
-        if((numbers[j] != numbers[max_index1]) && ((max_index2 == -1) || (numbers[j] > numbers[max_index2])))
+        if((j != max_index1) && ((max_index2 == -1) || (numbers[j] > numbers[max_index2])))
         {
             max_index2 = j;
         }
@@ -52,6 +53,38 @@ long long MaxPairwiseProductFast(const vector<int>& numbers)
 
 int main()
 {
+    //STRESS TEST IMPLEMENTATION BELOW:
+    /*
+    while(true)
+    {
+        int n = rand() % 10 + 2;
+        cout << n << "\n";
+        vector<int> a;
+
+        for(int i = 0; i < n; ++i)
+        {
+            a.push_back(rand() % 100000);
+        }
+
+        for(int i = 0; i < n; ++i)
+        {
+            cout << a[i] << ' ';
+        }
+
+        cout << "\n";
+        long long res1 = MaxPairwiseProduct(a);
+        long long res2 = MaxPairwiseProductFast(a);
+
+        if(res1 != res2)
+        {
+            cout << "Wrong answer: " << res1 << ' ' << res2 << "\n";
+            break;
+        }
+        else{
+            cout << "OK\n";
+        }
+    }
+*/
     int n;
     cin >> n;
     vector<int> numbers(n);
