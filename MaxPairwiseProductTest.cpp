a7ad74f8258d1ca5bb5731c5b990bfaa33bbe2ec
sarthakgup <16374350+sarthakgup@users.noreply.github.com>
2021-06-20 17:50:55 -0700

    implemented faster version maxPairwiseProductFast

--- MaxPairwiseProductTest.cpp
+++ MaxPairwiseProductTest.cpp
@@ -23,6 +23,32 @@ long long MaxPairwiseProduct(const vector<int>& numbers)
     return result;
 }
 
+long long MaxPairwiseProductFast(const vector<int>& numbers)
+{
+    int n = numbers.size();
+
+    int max_index1 = -1;
+
+    for(int i = 0; i < n; ++i)
+    {
+        if((max_index1 == -1) || (numbers[i]> numbers[max_index1]))
+        {
+            max_index1 = i;
+        }
+    }
+
+    int max_index2 = -1;
+
+    for(int j = 0; j < n; ++j)
+    {
+        if((numbers[j] != numbers[max_index1]) && ((max_index2 == -1) || (numbers[j] > numbers[max_index2])))
+        {
+            max_index2 = j;
+        }
+    }
+
+    return ((long long)(numbers[max_index1])) * numbers[max_index2];
+}
 
 int main()
 {
@@ -34,7 +60,7 @@ int main()
         cin >> numbers[i];
     }
 
-    long long result = MaxPairwiseProduct(numbers);
+    long long result = MaxPairwiseProductFast(numbers);
     cout << result << "\n";
     return 0;
 }
